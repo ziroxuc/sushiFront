6e2007cfdb1e99caf1a6bd4af2cb6c73dda51412
ziroxuc <mdgarcia.alarcon>
2017-09-23 21:04:15 -0300

    Carro de compras solucionado

--- src/app/services/carrito-compra.service.ts
+++ src/app/services/carrito-compra.service.ts
@@ -8,47 +8,45 @@ export class CarritoCompraService {
   constructor() { }
 
   carroCompra: IProductos[] = [];
-  cantidadItems:number = 0;
-
-//metodo que modifica la cantidad de carrito de compra del nav bar (numero rojo)
-  modificaCantidad(){
-    let cont:number = 0;
-    for (let item of this.carroCompra) {
-      cont = cont + item.cantidad;
-    }
-    this.cantidadItems = cont;
-  }
 
 //metodo que rescata del local storage la cantidad de items en el nav bar
-  getCantidadCarro(){
-     if (sessionStorage.getItem("cantEnCarro")){
-      this.cantidadItems = parseInt(sessionStorage.getItem("cantEnCarro"));
+ getCantidadCarro(){
+
+    let cantItems = 0;
+    for (let item of this.carroCompra) { 
+      cantItems = cantItems + item.cantidad;
     }
-    return this.cantidadItems;
+    return cantItems;
   }
 
   addCarrito(sushiObj: IProductos) {
 
-    for (let item of this.carroCompra) {
+   if (this.carroCompra.length < 1){
+        sushiObj.cantidad = 1;
+        this.carroCompra.push(sushiObj);
+        this.grabarLocalStorge();
+        return;
+  }
+  if (this.carroCompra.length > 0){
+
+    for (let item of this.carroCompra) {    
       if (item.id_sushi == sushiObj.id_sushi) {
         
         item.cantidad++;
-        this.cantidadItems++;
         this.grabarLocalStorge();
-         $('#ModalDetalle').modal('hide');
         return;
       }
     }
+    sushiObj.cantidad = 1;
     this.carroCompra.push(sushiObj);
-    this.cantidadItems++;
     this.grabarLocalStorge();
-    $('#ModalDetalle').modal('hide');
-    
+
+    } 
   }
 
    grabarLocalStorge(){
     sessionStorage.setItem("carro",JSON.stringify(this.carroCompra));
-    sessionStorage.setItem("cantEnCarro", this.cantidadItems.toString());
+   // sessionStorage.setItem("cantEnCarro", this.cantidadItems.toString());
   }
 
   cargarLocalStorage(){
@@ -78,8 +76,7 @@ export class CarritoCompraService {
   deleteCarrito(id: number, sushiObj: IProductos) {
 
       this.carroCompra.splice(id, 1);
-      sushiObj.cantidad = 1;
-      this.modificaCantidad();
+      sushiObj.cantidad = 0;
       this.grabarLocalStorge();
   }
 
@@ -88,16 +85,12 @@ export class CarritoCompraService {
   if(suma == true && sushiObj.cantidad > 0){
     
       sushiObj.cantidad++;
-      this.cantidadItems++;
-      
       this.grabarLocalStorge();
     }
     
     if(suma == false){
       if(sushiObj.cantidad > 1){
          sushiObj.cantidad--;
-         this.cantidadItems--;
-         
          this.grabarLocalStorge();
       }
 
